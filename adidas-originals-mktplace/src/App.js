2926b50d343177f304e2da9ea2c0c6d34c4db3b9
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-09 16:34:52 -0300

    working on

--- adidas-originals-mktplace/src/App.js
+++ adidas-originals-mktplace/src/App.js
@@ -26,7 +26,8 @@ function App() {
           <img src="https://images.squarespace-cdn.com/content/v1/5cd2555994d71a16b3b6ffc1/c4013b7c-2f74-41f0-a11e-8b28a48b4be0/346898663_145124835115329_5428517613208174571_n.jpg" className="harold" alt="" />
           <div className="slogans">
             <h3 className="slogan-1">Back to the originals with <span className='satellite'>SATELLITE STROMPPERS</span></h3>
-            <p>UP TO 60% OFF on the best of three stripes.</p>
+            <p>UP TO 60% OFF on the best of three stripes.</p> <br />
+            <button className='buy-now-btn'>BUY NOW</button>
             {/* <h1 className="slogan-2">SATELLITE STROMPPERS</h1> */}
           </div>
         </div>
