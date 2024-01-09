a4ff7219142a0c9ec63625583025dca2bca34e82
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-09 15:47:01 -0300

    working on

--- adidas-originals-mktplace/src/App.js
+++ adidas-originals-mktplace/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="header-container">
+        <img src="https://logodownload.org/wp-content/uploads/2019/11/adidas-originals-logo.png" className="logo-pic" alt="" />
+        <nav className="menu-options">
+          <a href="#gazelle">GAZELLE</a>
+          <a href="#spezial">SPEZIAL</a>
+          <a href="#samba">SAMBA</a>
+          <a href="#campus">CAMPUS</a>
+          <input placeholder="Search" className="searchBar"/>
+          <button type="button" className="searchButton">
+            <img src="https://icones.pro/wp-content/uploads/2021/06/icone-loupe-gris.png" className="lupe"alt="" />
+          </button>
+        </nav>
+        <button>
+          <div className="btn-face-blue">
+            Login
+          </div>
+          </button>
       </header>
-    </div>
+      <div className="content-container">
+        <div className="image-container">
+          <img src="https://images.squarespace-cdn.com/content/v1/5cd2555994d71a16b3b6ffc1/c4013b7c-2f74-41f0-a11e-8b28a48b4be0/346898663_145124835115329_5428517613208174571_n.jpg" className="harold" alt="" />
+          <div className="slogans">
+            <h3 className="slogan-1">Back to the originals with</h3>
+            <h1 className="slogan-2">SATELLITE STROMPPERS</h1>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
